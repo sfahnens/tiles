98f9c655b95252e97073b60024188efcdd54672c
Sebastian Fahnenschreiber <sebastian.fahnenschreiber@gmail.com>
2017-05-01 19:01:20 +0200

    wip MOTIS compat

--- include/tiles/database.h
+++ include/tiles/database.h
@@ -20,22 +20,30 @@ inline void checked(rocksdb::Status&& status) {
 using spatial_db_ptr = std::unique_ptr<rocksdb::spatial::SpatialDB>;
 
 inline spatial_db_ptr open_spatial_db(std::string const& path,
-                               bool read_only = false) {
+                                      bool read_only = false) {
+  return {};
+  // using namespace rocksdb::spatial;
+
+  // SpatialDB* db;
+  // checked(SpatialDB::Open(SpatialDBOptions(), path, &db, read_only));
+
+  // return std::unique_ptr<rocksdb::spatial::SpatialDB>(db);
+}
+
+inline rocksdb::Status create_spatial_db(
+    std::string const& path, std::vector<rocksdb::ColumnFamilyDescriptor> const&
+                                 extra_column_families = {}) {
   using namespace rocksdb::spatial;
 
-  SpatialDB* db;
-  checked(SpatialDB::Open(SpatialDBOptions(), path, &db, read_only));
-
-  return std::unique_ptr<rocksdb::spatial::SpatialDB>(db);
+  return SpatialDB::Create(
+      SpatialDBOptions(), path,
+      {SpatialIndexOptions("zoom10", bbox(proj::tile_bounds_merc(0, 0, 0)),
+                           10)},
+      extra_column_families);
 }
 
 inline void init_spatial_db(std::string const& path) {
-  using namespace rocksdb::spatial;
-
-  checked(SpatialDB::Create(
-      SpatialDBOptions(), path,
-      {SpatialIndexOptions("zoom10", bbox(proj::tile_bounds_merc(0, 0, 0)),
-                           10)}));
+  checked(create_spatial_db(path));
 }
 
 }  // namespace tiles

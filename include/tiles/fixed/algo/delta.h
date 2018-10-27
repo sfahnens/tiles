ec393f71625046ba816a934609d6513649a93a9b
Sebastian Fahnenschreiber <sebastian.fahnenschreiber@gmail.com>
2018-10-27 11:42:45 +0200

    fixed: add missing method

--- include/tiles/fixed/algo/delta.h
+++ include/tiles/fixed/algo/delta.h
@@ -9,6 +9,8 @@ namespace tiles {
 struct delta_encoder {
   explicit delta_encoder(fixed_coord_t init) : curr_(init) {}
 
+  void reset(fixed_coord_t val) { curr_ = val; }
+
   fixed_delta_t encode(fixed_coord_t val) {
     // implementation defined: works as expected in clang and gcc
     auto delta = static_cast<fixed_delta_t>(val - curr_);

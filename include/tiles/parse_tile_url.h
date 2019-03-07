a007dfea3ffd429d29cd7cd4e3cde3768dab583d
Sebastian Fahnenschreiber <fahnenschreiber@algo.informatik.tu-darmstadt.de>
2019-03-07 09:49:55 +0100

    add parse_tile_url

---
+++ include/tiles/parse_tile_url.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "geo/tile.h"
+
+namespace tiles {
+
+std::optional<geo::tile> parse_tile_url(std::string const& url) {
+  std::regex regex{"/(\\d+)\\/(\\d+)\\/(\\d+).mvt$"};
+  std::smatch match;
+  if (!std::regex_search(url, match, regex) || match.size() != 4) {
+    return {};
+  }
+
+  return geo::tile{static_cast<uint32_t>(std::stoul(match[2])),
+                   static_cast<uint32_t>(std::stoul(match[3])),
+                   static_cast<uint32_t>(std::stoul(match[1]))};
+}
+
+}  // namespace tiles

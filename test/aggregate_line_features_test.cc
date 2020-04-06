08d21d0245416ad9b38a1a452ff57afc000b4fb3
Sebastian Fahnenschreiber <fahnenschreiber@algo.informatik.tu-darmstadt.de>
2020-04-06 14:28:01 +0200

    fix aggregate_line_features_test

--- test/aggregate_line_features_test.cc
+++ test/aggregate_line_features_test.cc
@@ -14,7 +14,7 @@ TEST_CASE("aggregate_line_features") {
     f2.id_ = 2;
     f2.geometry_ = tiles::fixed_polyline{{{11, 11}, {12, 12}}};
 
-    auto result = tiles::aggregate_line_features({f1, f2});
+    auto result = tiles::aggregate_line_features({f1, f2}, 20);
     REQUIRE(result.size() == 1);
 
     auto geo = mpark::get<tiles::fixed_polyline>(result.at(0).geometry_);
@@ -35,7 +35,7 @@ TEST_CASE("aggregate_line_features") {
     f2.id_ = 2;
     f2.geometry_ = tiles::fixed_polyline{{{12, 12}, {11, 11}}};
 
-    auto result = tiles::aggregate_line_features({f1, f2});
+    auto result = tiles::aggregate_line_features({f1, f2}, 20);
     REQUIRE(result.size() == 1);
 
     auto geo = mpark::get<tiles::fixed_polyline>(result.at(0).geometry_);
@@ -56,7 +56,7 @@ TEST_CASE("aggregate_line_features") {
     f2.id_ = 2;
     f2.geometry_ = tiles::fixed_polyline{{{12, 12}, {10, 10}}};
 
-    auto result = tiles::aggregate_line_features({f1, f2});
+    auto result = tiles::aggregate_line_features({f1, f2}, 20);
     REQUIRE(result.size() == 1);
 
     auto geo = mpark::get<tiles::fixed_polyline>(result.at(0).geometry_);
@@ -77,7 +77,7 @@ TEST_CASE("aggregate_line_features") {
     f2.id_ = 2;
     f2.geometry_ = tiles::fixed_polyline{{{10, 10}, {12, 12}}};
 
-    auto result = tiles::aggregate_line_features({f1, f2});
+    auto result = tiles::aggregate_line_features({f1, f2}, 20);
     REQUIRE(result.size() == 1);
 
     auto geo = mpark::get<tiles::fixed_polyline>(result.at(0).geometry_);
@@ -102,7 +102,7 @@ TEST_CASE("aggregate_line_features") {
     f3.id_ = 3;
     f3.geometry_ = tiles::fixed_polyline{{{12, 12}, {13, 13}}};
 
-    auto result = tiles::aggregate_line_features({f1, f2, f3});
+    auto result = tiles::aggregate_line_features({f1, f2, f3}, 20);
     REQUIRE(result.size() == 1);
 
     auto geo = mpark::get<tiles::fixed_polyline>(result.at(0).geometry_);
@@ -114,6 +114,4 @@ TEST_CASE("aggregate_line_features") {
     CHECK(geo.front()[2] == tiles::fixed_xy(12, 12));
     CHECK(geo.front()[3] == tiles::fixed_xy(13, 13));
   }
-
-
 }

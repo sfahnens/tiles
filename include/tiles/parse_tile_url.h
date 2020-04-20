d712bdbdb1c96b953d5b82f6964442ede60f5a81
Sebastian Fahnenschreiber <fahnenschreiber@algo.informatik.tu-darmstadt.de>
2020-04-20 18:30:34 +0200

    fix includes

--- include/tiles/parse_tile_url.h
+++ include/tiles/parse_tile_url.h
@@ -1,10 +1,13 @@
 #pragma once
 
+#include <regex>
+#include <optional>
+
 #include "geo/tile.h"
 
 namespace tiles {
 
-std::optional<geo::tile> parse_tile_url(std::string const& url) {
+inline std::optional<geo::tile> parse_tile_url(std::string const& url) {
   std::regex regex{"/(\\d+)\\/(\\d+)\\/(\\d+).mvt$"};
   std::smatch match;
   if (!std::regex_search(url, match, regex) || match.size() != 4) {

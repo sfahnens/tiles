fbb4d9b19784d7f9fb48f5ba82901106584b34f8
Sebastian Fahnenschreiber <fahnenschreiber@algo.informatik.tu-darmstadt.de>
2020-04-27 12:28:04 +0200

    client: tweak styles, add shields

--- client/style-default.js
+++ client/style-default.js
@@ -1,3 +1,59 @@
+function createShield(opt) {
+  const d = 32
+
+  const cv = document.createElement('canvas');
+  cv.width = d;
+  cv.height = d;
+  const ctx = cv.getContext('2d');
+
+  // coord of the line (front = near zero, back = opposite)
+  const l_front = 1;
+  const l_back = d-1;
+
+  // coord start of the arc
+  const lr_front = l_front + 2;
+  const lr_back = l_back - 2;
+
+  // control point of the arc
+  const lp_front = l_front + 1;
+  const lp_back = l_back - 1;
+
+  let p = new Path2D();
+  p.moveTo(lr_front, l_front);
+
+  // top line
+  p.lineTo(lr_back, l_front);
+  // top right corner
+  p.bezierCurveTo(lp_back, lp_front, lp_back, lp_front, l_back, lr_front);
+  // right line
+  p.lineTo(l_back, lr_back);
+  // bottom right corner
+  p.bezierCurveTo(lp_back, lp_back, lp_back, lp_back, lr_back, l_back);
+  // bottom line
+  p.lineTo(lr_front, l_back);
+  // bottom left corner
+  p.bezierCurveTo(lp_front, lp_back, lp_front, lp_back, l_front, lr_back);
+  // left line
+  p.lineTo(l_front, lr_front);
+  // top left corner
+  p.bezierCurveTo(lp_front, lp_front, lp_front, lp_front, lr_front, l_front);
+
+  p.closePath();
+
+  ctx.fillStyle = opt.fill;
+  ctx.fill(p);
+  ctx.strokeStyle = opt.stroke;
+  ctx.stroke(p);
+
+  return [
+    ctx.getImageData(0, 0, d, d),
+    {
+      content: [lr_front, lr_front, lr_back, lr_back],
+      stretchX: [[lr_front, lr_back]],
+      stretchY: [[lr_front, lr_back]]
+    }
+  ];
+}
 
 const water = "#bbd3f9";
 const rail = "#5b5b5b";
@@ -11,7 +67,8 @@ const sport_outline = "#94a7b7";
 const building = "#d8d8d8";
 const building_outline = "#c1c1c1";
 
-export const style = {
+export const style = (map) => {
+  map.setStyle({
     "version": 8,
     "sources": {
         "osm": {
@@ -200,22 +257,29 @@ export const style = {
             "source": "osm",
             "source-layer": "road",
             "minzoom": 6,
-            "filter": ["any", ["==", ["get", "highway"], "motorway"],
-                              ["==", ["get", "highway"], "trunk"],
-                              ["==", ["get", "highway"], "secondary"],
-                              [">", ["zoom"], 11]],
+            "filter": ["all",
+                        ["has", "ref"],
+                        ["any", ["==", ["get", "highway"], "motorway"],
+                                ["==", ["get", "highway"], "trunk"],
+                                ["==", ["get", "highway"], "secondary"],
+                                [">", ["zoom"], 11]]],
             "layout": {
               "symbol-placement": "line",
               "text-field": ["get", "ref"],
               "text-font": ["Noto Sans Display Regular"],
-              "text-size": 10,
-              "text-rotation-alignment": "viewport"
+              "text-size": ["case", ["==", ["get", "highway"], "motorway"], 11, 10],
+              "text-justify": "center",
+              "text-rotation-alignment": "viewport",
+              "text-pitch-alignment": "viewport",
+              "icon-image": "shield",
+              "icon-text-fit": "both",
+              "icon-text-fit-padding": [.5, 4, .5, 4],
+              "icon-rotation-alignment": "viewport",
+              "icon-pitch-alignment": "viewport",
             },
             "paint": {
-              "text-halo-width": 10,
-              "text-halo-color": "white",
-              "text-color": "#333333"
-            }
+              "text-color": "#333333",
+            },
         }, {
             "id": "road-name-text",
             "type": "symbol",
@@ -270,4 +334,13 @@ export const style = {
             }
         }
     ]
+  });
+
+  map.addImage(
+    "shield",
+    ...createShield({
+      fill: "hsl(0, 0%, 98%)",
+      stroke: "hsl(0, 0%, 75%)",
+    })
+  );
 };

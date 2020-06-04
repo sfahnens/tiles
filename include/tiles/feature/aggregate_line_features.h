f5bb1fe47aab1f56f18dc7ca08b849b2d6a0270a
Sebastian Fahnenschreiber <fahnenschreiber@algo.informatik.tu-darmstadt.de>
2020-06-04 15:35:05 +0200

    fix formatting

--- include/tiles/feature/aggregate_line_features.h
+++ include/tiles/feature/aggregate_line_features.h
@@ -7,7 +7,6 @@ namespace tiles {
 
 struct feature;
 
-std::vector<feature> aggregate_line_features(std::vector<feature>,
-                                             uint32_t z);
+std::vector<feature> aggregate_line_features(std::vector<feature>, uint32_t z);
 
 }  // namespace tiles
